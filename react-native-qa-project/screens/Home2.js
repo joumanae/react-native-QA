9a9645676ba22b42ede765ee13bda25308d643c1
joumanae <joumana.codes@gmail.com>
2020-04-10 10:09:55 -0500

    test commit

---
+++ react-native-qa-project/screens/Home2.js
@@ -0,0 +1,87 @@
+import React, { useState, useEffect, useCallback } from 'react';
+import { StyleSheet, FlatList, Text, TouchableOpacity } from 'react-native';
+import PalettePreview from '../components/PalettePreview';
+
+const Home = ({ navigation, route }) => {
+  const newColorPalette = route.params
+    ? route.params.newColorPalette
+    : undefined;
+  const [colorPalettes, setColorPalettes] = useState([]);
+  const [isRefreshing, setIsRefreshing] = useState(false);
+
+  const fetchColorPalettes = useCallback(async () => {
+    const result = await fetch(
+      'https://color-palette-api.kadikraman.now.sh/palettes',
+    );
+    if (result.ok) {
+      const palettes = await response.json();
+      setColorPalettes(palettes);
+    }
+  }, []);
+
+  useEffect(() => {
+    fetchColorPalettes();
+  }, []);
+
+  const handleRefresh = useCallback(async () => {
+    setIsRefreshing(true);
+    await fetchColorPalettes();
+    setTimeout(() => {
+      setIsRefreshing(false);
+    }, 1000);
+  }, []);
+
+  useEffect(() => {
+    if (newColorPalette) {
+      setColorPalettes(palettes => [newColorPalette, ...palettes]);
+    }
+  }, [newColorPalette]);
+
+  return (
+    <FlatList
+      style={styles.list}
+      data={colorPalettes}
+      keyExtractor={item => item.paletteName}
+      renderItem={({ item }) => (
+        <PalettePreview
+          handlePress={() => {
+            navigation.navigate('ColorPalette', item);
+          }}
+          colorPalette={item}
+        />
+      )}
+      refreshing={isRefreshing}
+      onRefresh={handleRefresh}
+      ListHeaderComponent={
+        <TouchableOpacity
+          onPress={() => {
+            navigation.navigate('ColorPaletteModal');
+          }}
+        >
+          <Text>Launch Modal</Text>
+        </TouchableOpacity>
+      }
+    />
+  );
+};
+
+const styles = StyleSheet.create({
+  list: {
+    flex: 1,
+    padding: 10,
+    backgroundColor: 'white',
+  },
+  button: {
+    height: 50,
+    backgroundColor: 'white',
+    padding: 10,
+  },
+  buttonText: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: 'teal',
+    marginBottom: 10,
+  },
+});
+
+export default Home;
